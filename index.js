205e61104e75d70103ada37b85e62618cb262d05
Hisham <hmuhammed546@gmail.com>
2023-10-27 11:00:01 +0530

    added a basic get request

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import { PORT } from "./src/config.js";
 const app = express();
 //Middleware for parsing request body
 app.use(express.json());
+app.get('/', (request, response) => {
 
+    console.log(request)
+    return response.status(234).send('Welcome to FastPay')
+});
 app.listen(PORT, () => {
     console.log(`App is listening to port :${PORT}`)
 
