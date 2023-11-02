71a7f92b6456858b266fdc81c470d6a77564de17
Hisham <hmuhammed546@gmail.com>
2023-11-02 11:00:23 +0530

    added db url

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://Hisham:1XmdmSd163VJdDjc@wallet.hmlpkzv.mongodb.net/?retryWrites=true&w=majority'
+//1XmdmSd163VJdDjc password
+//Hisham username
